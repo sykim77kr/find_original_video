a45fe5d0d548cb986562cf3bd9540b7a1082d4c8
Soyeon Kim <sykim77kr@naver.com>
2018-11-20 20:54:23 +0900

    second commit

--- find_video.cpp
+++ find_video.cpp
@@ -70,18 +70,25 @@ void analyzeData(void)
 
 	while (true) {
 		Mat frame, roi;
+		int width, height;
 		video.read(frame);
 		if (frame.empty())
 			break;
-		roi = frame(Rect((frame.size().width) / 3, (frame.size().height) / 3,
-			(frame.size().width) / 3, (frame.size().height) / 3));
+		width = frame.size().width / 3;
+		height = frame.size().height / 3;
 
-		coordinates = getContour(roi, db_cnt);
-		getAverage(coordinates, db_cnt, db_m_x, db_m_y);
-		// 평균 db에 저장하기
-
-		getVariance(coordinates, db_cnt, db_m_x, db_m_y, db_v_x, db_v_y);
-		// 분산 db에 저장하기
+		for (int i = 0; i <= frame.size().height - height; i += height) {
+			for (int j = 0; j <= frame.size().width - width; j += width) {
+				roi = frame(Rect(j, i, width, height));
+		
+				coordinates = getContour(roi, db_cnt);
+				getAverage(coordinates, db_cnt, db_m_x, db_m_y);
+				// 평균 db에 저장하기
+		
+				getVariance(coordinates, db_cnt, db_m_x, db_m_y, db_v_x, db_v_y);
+				// 분산 db에 저장하기
+			}
+		}
 
 		imshow("frame", frame);
 		imshow("roi", roi);
